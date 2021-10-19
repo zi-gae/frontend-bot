8c88eff5ec30d54a7c53a38f2103a69ebf227ce3
geonwoo.jeong <doscmdev@gmail.com>
2021-10-19 17:28:23 +0900

    ci: test

--- src/utils/canaryBodyParser.ts
+++ src/utils/canaryBodyParser.ts
@@ -2,8 +2,10 @@ export function canaryBodyParser(body: string) {
   const regex = /```bash.*?```/s;
   const pars = body.match(regex);
 
-  console.log("RPEV", pars);
-  const result = pars?.[0].replace("bash", "").replaceAll("  ", "");
+  console.log("PREV", pars);
+
+  const result = pars?.[0];
+  // .replace("bash", "").replaceAll("  ", "");
   console.log("AFTER", result);
 
   return result;

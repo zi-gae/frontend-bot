4a9f130ab4f882a7b28a6b53367bbc9710c336b2
geonwoo.jeong <doscmdev@gmail.com>
2021-10-19 17:31:03 +0900

    ci: test

--- dist/index.js
+++ dist/index.js
@@ -20206,8 +20206,9 @@ exports.canaryBodyParser = void 0;
 function canaryBodyParser(body) {
     const regex = /```bash.*?```/s;
     const pars = body.match(regex);
-    console.log("RPEV", pars);
-    const result = pars?.[0].replace("bash", "").replaceAll("  ", "");
+    console.log("PREV", pars);
+    const result = pars?.[0];
+    // .replace("bash", "").replaceAll("  ", "");
     console.log("AFTER", result);
     return result;
 }

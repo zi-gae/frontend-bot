cc2d54698d4e69944fc66d3a3d7311e59308430f
geonwoo.jeong <doscmdev@gmail.com>
2021-10-20 14:46:58 +0900

    ci: test

--- dist/index.js
+++ dist/index.js
@@ -20333,16 +20333,15 @@ const tslib_1 = __nccwpck_require__(4351);
 const web_api_1 = __nccwpck_require__(431);
 const input_1 = __nccwpck_require__(5073);
 const canaryBodyParser_1 = __nccwpck_require__(116);
-const dummy = "### Changes\r\n\r\n### Check points\r\n- [ ] 브라우저 호환성 테스트\r\n\r\n### Notes\r\n\r\n### Post Actions\r\n\n<!-- GITHUB_RELEASE PR BODY: canary-version -->\n<details>\n  <summary>📦 Published PR as canary version: <code>2.0.2-canary.1.29ff6c6b9df189bfd673f1c840878282560ea0e9.0</code></summary>\n  <br />\n  \n  :sparkles: Test out this PR locally via:\n  \n  ```bash\n  npm install @zi-gae/design-system@2.0.2-canary.1.29ff6c6b9df189bfd673f1c840878282560ea0e9.0\n  # or \n  yarn add @zi-gae/design-system@2.0.2-canary.1.29ff6c6b9df189bfd673f1c840878282560ea0e9.0\n  ```\n</details>\n<!-- GITHUB_RELEASE PR BODY: canary-version -->\n";
 const slackClient = new web_api_1.WebClient(input_1.SLACK_BOT_TOKEN);
 function sendMessage(args) {
     return slackClient.chat.postMessage(args);
 }
 exports.sendMessage = sendMessage;
-function sendCanaryPublishMessage({ comment: { link }, }) {
+function sendCanaryPublishMessage({ comment: { link, body }, }) {
     return (0, tslib_1.__awaiter)(this, void 0, void 0, function* () {
         const header = ":sparkles: 다음을 통해 PR 로컬 테스트:\n";
-        const content = (0, canaryBodyParser_1.canaryBodyParser)(dummy);
+        const content = (0, canaryBodyParser_1.canaryBodyParser)(body);
         console.log("content", content);
         const blocks = [
             {
--- src/utils/slack.ts
+++ src/utils/slack.ts
@@ -3,9 +3,6 @@ import { SLACK_BOT_TOKEN, TARGET_SLACK_CHANNEL_ID } from "./input";
 import { GithubComment } from "../models/github";
 import { canaryBodyParser } from "./canaryBodyParser";
 
-const dummy =
-  "### Changes\r\n\r\n### Check points\r\n- [ ] 브라우저 호환성 테스트\r\n\r\n### Notes\r\n\r\n### Post Actions\r\n\n<!-- GITHUB_RELEASE PR BODY: canary-version -->\n<details>\n  <summary>📦 Published PR as canary version: <code>2.0.2-canary.1.29ff6c6b9df189bfd673f1c840878282560ea0e9.0</code></summary>\n  <br />\n  \n  :sparkles: Test out this PR locally via:\n  \n  ```bash\n  npm install @zi-gae/design-system@2.0.2-canary.1.29ff6c6b9df189bfd673f1c840878282560ea0e9.0\n  # or \n  yarn add @zi-gae/design-system@2.0.2-canary.1.29ff6c6b9df189bfd673f1c840878282560ea0e9.0\n  ```\n</details>\n<!-- GITHUB_RELEASE PR BODY: canary-version -->\n";
-
 const slackClient = new WebClient(SLACK_BOT_TOKEN);
 
 export function sendMessage(args: ChatPostMessageArguments) {
@@ -13,13 +10,13 @@ export function sendMessage(args: ChatPostMessageArguments) {
 }
 
 export async function sendCanaryPublishMessage({
-  comment: { link },
+  comment: { link, body },
 }: {
   comment: GithubComment;
 }) {
   const header = ":sparkles: 다음을 통해 PR 로컬 테스트:\n";
 
-  const content = canaryBodyParser(dummy);
+  const content = canaryBodyParser(body);
   console.log("content", content);
 
   const blocks = [
